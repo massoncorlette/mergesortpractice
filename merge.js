0b1f47b5d5d172fa4bbf2c32f4520ee7d1e7fb0a
massoncorlette <massoncorlette07@gmail.com>
2024-09-12 01:35:50 -0400

    I spent many hours going through the recursion exercises, while I did look up some solutions - I made sure to understand why they work. Recursion is becoming more clear now.

--- merge.js
+++ merge.js
@@ -20,18 +20,30 @@ function fibs(fibSize) {
   return result;
 }
 
+//fibsRec(n)
+//  resultArray
+//  currentNum = 0
+//  
+//  if(n =0) return array
+//   
+//  currentNum += fibsRec(n)
+//  return result.push(fibsRec(n -1))
+
 
 // array.shift ??
 function fibsRec(fibSize) {
 
-  let result = [];
-
-  if (fibSize === 1) {
-    result.push[0];
-    return result;
-  }
+    if (fibSize <= 0) return [];
+    if (fibSize === 1) return [0];
+    if (fibSize === 2) return [0, 1];
   
+    const result = fibsRec(fibSize - 1);
+    result.push(result[result.length - 1] + result[result.length - 2]);
+  
+    return result;
+
 }
+console.log(fibsRec(2));
 
 function sumRange(num) {
   if(num == 1) return 1;
@@ -46,10 +58,32 @@ function factorial(num) {
   return num = num * factorial(num - 1);
 }
 
+
+// recursion example of doing through a complex array 
+var seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
+//console.log(seven);
+
+function totalIntegers(array){
+	if(array.length === 0) return 0;
+
+	let total = 0;
+	let first = array.shift();
+
+	if (Array.isArray(first)){
+		total += totalIntegers(first); 
+	} else if (Number.isInteger(first)) {
+		total += 1;
+	}
+
+	return total + totalIntegers(array);
+}
+
 //console.log(fibs(8));
 
-console.log(fibsRec(5));
 
-console.log(sumRange(5));
 
-console.log(factorial(5));
+//console.log(sumRange(4));
+
+//console.log(factorial(5));
+
+//console.log(factorial(5));
